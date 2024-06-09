2a136773c3efd4057ff9a75b400f5e054ff12247
Degraft Frimpong <frimpsup@gmail.com>
2024-06-09 19:49:52 -0400

    generateTransaction added

--- random-transaction.js
+++ random-transaction.js
@@ -35,3 +35,49 @@ const createCoinbaseTransaction=()=>{
         }]
     }
 }
+
+const selectInput=(amountNeeded)=>{
+    const inputs=[];
+    let totalselected=0;
+    const threasholdMultiplier=1+(Math.random() * 1);
+    const targetAmount=amountNeeded*threasholdMultiplier;
+
+    for(const [txid,{address,amount}] of spentOutput){
+        if(totalselected>=targetAmount) break;
+        inputs.push({txid,address});
+        totalselected+=amount;
+        spentOutput.delete(txid);
+
+    }
+
+    return {inputs,totalselected};
+}
+
+
+
+const generateTransaction=()=>{
+    const txid=generateTxId();
+    const amountNeeded=getRandomInt(1,100)
+    const {inputs,totalselected}=selectInput(amountNeeded);
+    if(inputs.length===0){
+        return null;
+    }
+    const outputs=[];
+    outputs.push({address:generateAddress(),amount:amountNeeded});
+    let remainingAmount=totalselected-amountNeeded;
+    const numberOfChangeAddress=getRandomInt(1,3)
+    for(let i=0;i<numberOfChangeAddress;i++){
+        const AmountForThisAddress=(i===numberOfChangeAddress-1)?remainingAmount:Math.floor(remainingAmount/(numberOfChangeAddress-1))
+        if(AmountForThisAddress>0){
+            outputs.push({address:generateAddress(),amount:AmountForThisAddress});
+            remainingAmount-=AmountForThisAddress;
+        }
+        outputs.forEach(output=>{
+            spentOutput.set(txid,{address:output.address,amount:output.amount})
+        })
+
+        return{
+            txid,timeStamp:generateTimestamp(),inputs,outputs
+        }
+    }
+}
